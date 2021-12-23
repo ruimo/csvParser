7aece438b00f129cf99fc0f5628842952ccf9d31
Shisei Hanai <shanai@jp.ibm.com>
2021-12-23 10:42:30 +0900

    Use scalatest.

--- build.sbt
+++ build.sbt
@@ -1,27 +1,41 @@
-name := """csvParser"""
+import ReleaseTransformations._
 
-organization := "com.ruimo"
-
-scalaVersion := "2.12.3"
-
-crossScalaVersions := List("2.11.8", "2.12.3") 
-
-resolvers += "ruimo.com" at "http://static.ruimo.com/release"
-
-publishTo := Some(
-  Resolver.file(
-    "recoengcommon",
-    new File(Option(System.getenv("RELEASE_DIR")).getOrElse("/tmp"))
+lazy val root = project
+  .in(file("."))
+  .settings(
+    name := """csvParser""",
+    homepage := Some(url("https://github.com/ruimo/csvParser")),
+    organization := "com.ruimo",
+    description := "CSV parser",
+    sonatypeCredentialHost := "s01.oss.sonatype.org",
+    sonatypeRepository := "https://s01.oss.sonatype.org/service/local",
+    crossScalaVersions := List("2.11.12", "2.12.15", "2.13.7", "3.0.2"),
+    libraryDependencies ++= Seq(
+      "com.ruimo" %% "scoins" % "1.29",
+      "org.scalatest" %% "scalatest" % "3.2.10" % Test,
+      "org.scalactic" %% "scalactic" % "3.2.10" % Test,
+      "org.mockito" % "mockito-all" % "1.9.5" % Test
+    ),
+    scalacOptions := Seq("-unchecked", "-deprecation", "-feature"),
+    Test / scalacOptions ++= Seq("-Yrangepos"),
+    licenses := Seq(
+      "Apache-2.0" -> url("http://www.apache.org/licenses/LICENSE-2.0")
+    ),
+    developers := List(
+      Developer(
+        "ruimo",
+        "Shisei Hanai",
+        "ruimo.uno@gmail.com",
+        url("https://github.com/ruimo")
+      )
+    ),
+    scmInfo := Some(
+      ScmInfo(
+        url("https://github.com/ruimo/csvParser"),
+        "scm:git@github.com:ruimo/csvParser.git"
+      )
+    ),
+    sonatypeCredentialHost := "s01.oss.sonatype.org",
+    sonatypeRepository := "https://s01.oss.sonatype.org/service/local",
+    releaseProcess -= publishArtifacts
   )
-)
-
-libraryDependencies ++= Seq(
-  "org.specs2" %% "specs2-core" % "3.9.1" % "test",
-  "com.ruimo" %% "scoins" % "1.12"
-)
-
-scalacOptions in Test ++= Seq("-Yrangepos")
-
-resolvers ++= Seq("snapshots", "releases").map(Resolver.sonatypeRepo)
-
-resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
--- src/test/scala/com/ruimo/csv/ParserSpec.scala
+++ src/test/scala/com/ruimo/csv/ParserSpec.scala
@@ -1,323 +1,321 @@
 package com.ruimo.csv
 
-import org.specs2.mutable.Specification
 import com.ruimo.scoins.Scoping._
-import scala.util.{Try, Failure, Success}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+import scala.util.{Failure, Success, Try}
 import java.nio.file.Files
 import java.nio.charset.Charset
 
-class ParserSpec extends Specification {
-  "CSV parser" should {
-    "parse empty string" in {
-      Parser.parseOneLine("").get.size === 0
+class ParserSpec extends AnyFlatSpec with should.Matchers {
+  it should "parse empty string" in {
+    assert(Parser.parseOneLine("").get.size === 0)
+  }
+
+  it should "parse some string" in {
+    val cols = Parser.parseOneLine("abc").get
+    assert(cols.size === 1)
+    assert(cols(0) === "abc")
+  }
+
+  it should "parse quoted string" in {
+    val cols = Parser.parseOneLine("\"a b\"\"c\"").get
+    assert(cols.size === 1)
+    assert(cols(0) === "a b\"c")
+  }
+
+  it should "parse columns" in {
+    val cols = Parser.parseOneLine("\"a b\"\"c\",b,\"c\"").get
+    assert(cols.size === 3)
+    assert(cols(0) === "a b\"c")
+    assert(cols(1) === "b")
+    assert(cols(2) === "c")
+  }
+
+  it should "parse tab separated columns" in {
+    val cols = Parser.parseOneLine("\"a b\"\"c\"\tb\t\"c\"", '\t').get
+    assert(cols.size === 3)
+    assert(cols(0) === "a b\"c")
+    assert(cols(1) === "b")
+    assert(cols(2) === "c")
+  }
+
+  it should "tailing quote" in {
+    assertThrows[CsvParseException](Parser.parseOneLine("\"a b\"\"").get)
+  }
+
+  it should "non ended quote" in {
+    assertThrows[CsvParseException](Parser.parseOneLine("\"a b").get)
+  }
+
+  it should "parse one line" in {
+    val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,def".toIterator)
+    assert(z.hasNext === true)
+    val cols: Seq[String] = z.next.get
+    assert(cols.size === 2)
+    assert(cols(0) === "abc")
+    assert(cols(1) === "def")
+    assert(z.hasNext === false)
+  }
+
+  it should "parse one line with tab separated" in {
+    val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc\tdef".toIterator, '\t')
+    assert(z.hasNext === true)
+    val cols: Seq[String] = z.next.get
+    assert(cols.size === 2)
+    assert(cols(0) === "abc")
+    assert(cols(1) === "def")
+    assert(z.hasNext === false)
+  }
+
+  it should "parse one line with crlf" in {
+    val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,def\r\n".toIterator)
+    assert(z.hasNext === true)
+    val cols: Seq[String] = z.next.get
+    assert(cols.size === 2)
+    assert(cols(0) === "abc")
+    assert(cols(1) === "def")
+    assert(z.hasNext === false)
+  }
+
+  it should "parse one line with lf" in {
+    val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,def\n".toIterator)
+    assert(z.hasNext === true)
+    val cols: Seq[String] = z.next.get
+    assert(cols.size === 2)
+    assert(cols(0) === "abc")
+    assert(cols(1) === "def")
+    assert(z.hasNext === false)
+  }
+
+  it should "parse one line with quote and crlf" in {
+    val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,\"def\"\r\n".toIterator)
+    assert(z.hasNext === true)
+    val cols: Seq[String] = z.next.get
+    assert(cols.size === 2)
+    assert(cols(0) === "abc")
+    assert(cols(1) === "def")
+    assert(z.hasNext === false)
+  }
+  
+  it should "parse one line with quote and lf" in {
+    val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,\"def\"\n".toIterator)
+    assert(z.hasNext === true)
+    val cols: Seq[String] = z.next.get
+    assert(cols.size === 2)
+    assert(cols(0) === "abc")
+    assert(cols(1) === "def")
+    assert(z.hasNext === false)
+  }
+
+  it should "parse empty line" in {
+    val z: Iterator[Try[Seq[String]]] = Parser.parseLines("\r\n".toIterator)
+    assert(z.hasNext === true)
+    val cols: Seq[String] = z.next.get
+    assert(cols.size === 1)
+    assert(cols(0) === "")
+    assert(z.hasNext === false)
+  }
+
+  it should "parse col with crlf" in {
+    val z: Iterator[Try[Seq[String]]] = Parser.parseLines("\"ab\r\ncd\",b".toIterator)
+    assert(z.hasNext === true)
+    val cols: Seq[String] = z.next.get
+    assert(cols.size === 2)
+    assert(cols(0) === "ab\r\ncd")
+    assert(cols(1) === "b")
+    assert(z.hasNext === false)
+  }
+
+  it should "parse two lines" in {
+    val z: Iterator[Try[Seq[String]]] = Parser.parseLines("\"ab\r\ncd\",b\r\n\"de f\"".toIterator)
+    assert(z.hasNext === true)
+
+    doWith(z.next.get) { cols =>
+      assert(cols.size === 2)
+      assert(cols(0) === "ab\r\ncd")
+      assert(cols(1) === "b")
     }
 
-    "parse empty string" in {
-      val cols = Parser.parseOneLine("abc").get
-      cols.size === 1
-      cols(0) === "abc"
+    assert(z.hasNext === true)
+    doWith(z.next.get) { cols =>
+      assert(cols.size === 1)
+      assert(cols(0) === "de f")
     }
 
-    "parse quoted string" in {
-      val cols = Parser.parseOneLine("\"a b\"\"c\"").get
-      cols.size === 1
-      cols(0) === "a b\"c"
+    assert(z.hasNext === false)
+  }
+
+  it should "parse empty file" in {
+    import com.ruimo.scoins.LoanPattern.iteratorFromReader
+    import com.ruimo.csv.Parser.parseLines
+
+    val file = Files.createTempFile(null, ".csv")
+    val t = iteratorFromReader(Files.newBufferedReader(file, Charset.forName("Windows-31j"))) {
+      in: Iterator[Char] =>
+      val z: Iterator[Try[Seq[String]]] = parseLines(in)
+      assert(z.hasNext === false)
     }
+    assert(t.isSuccess === true)
+  }
 
-    "parse columns" in {
-      val cols = Parser.parseOneLine("\"a b\"\"c\",b,\"c\"").get
-      cols.size === 3
-      cols(0) === "a b\"c"
-      cols(1) === "b"
-      cols(2) === "c"
+  it should "parse blank line" in {
+    import com.ruimo.scoins.LoanPattern.iteratorFromReader
+    import com.ruimo.csv.Parser.parseLines
+    import java.util.Arrays.asList
+
+    val file = Files.createTempFile(null, ".csv")
+    Files.write(file, asList("", ""), Charset.forName("Windows-31j"))
+    val t = iteratorFromReader(Files.newBufferedReader(file, Charset.forName("Windows-31j"))) {
+      in: Iterator[Char] =>
+      val z: Iterator[Try[Seq[String]]] = parseLines(in)
+      assert(z.hasNext === true)
+      assert(z.next.get === Seq(""))
+
+      assert(z.hasNext === true)
+      assert(z.next.get === Seq(""))
+
+      assert(z.hasNext === false)
     }
+    assert(t.isSuccess === true)
+  }
 
-    "parse tab separated columns" in {
-      val cols = Parser.parseOneLine("\"a b\"\"c\"\tb\t\"c\"", '\t').get
-      cols.size === 3
-      cols(0) === "a b\"c"
-      cols(1) === "b"
-      cols(2) === "c"
+  it should "parse normal lines" in {
+    import com.ruimo.scoins.LoanPattern.iteratorFromReader
+    import com.ruimo.csv.Parser.parseLines
+    import java.util.Arrays.asList
+
+    val file = Files.createTempFile(null, ".csv")
+    Files.write(file, asList("a,\"b\"", "1,\"2 3\""), Charset.forName("Windows-31j"))
+    val t = iteratorFromReader(Files.newBufferedReader(file, Charset.forName("Windows-31j"))) {
+      in: Iterator[Char] =>
+      val z: Iterator[Try[Seq[String]]] = parseLines(in)
+      assert(z.hasNext === true)
+      assert(z.next.get === Seq("a", "b"))
+
+      assert(z.hasNext === true)
+      assert(z.next.get === Seq("1", "2 3"))
+
+      assert(z.hasNext === false)
     }
+    assert(t.isSuccess === true)
+  }
 
-    "tailing quote" in {
-      Parser.parseOneLine("\"a b\"\"").get must throwA[CsvParseException]
-    }
+  it should "parse error" in {
+    import com.ruimo.scoins.LoanPattern.iteratorFromReader
+    import com.ruimo.csv.Parser.parseLines
+    import java.util.Arrays.asList
 
-    "non ended quote" in {
-      Parser.parseOneLine("\"a b").get must throwA[CsvParseException]
-    }
+    val file = Files.createTempFile(null, ".csv")
+    Files.write(file, asList("a,\"b\"", "1,\"2 3"), Charset.forName("Windows-31j"))
+    val t = iteratorFromReader(Files.newBufferedReader(file, Charset.forName("Windows-31j"))) {
+      in: Iterator[Char] =>
+      val z: Iterator[Try[Seq[String]]] = parseLines(in)
+      assert(z.hasNext === true)
+      assert(z.next.get === Seq("a", "b"))
 
-    "parse one line" in {
-      val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,def".toIterator)
-      z.hasNext === true
-      val cols: Seq[String] = z.next.get
-      cols.size === 2
-      cols(0) === "abc"
-      cols(1) === "def"
-      z.hasNext === false
-    }
-
-    "parse one line with tab separated" in {
-      val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc\tdef".toIterator, '\t')
-      z.hasNext === true
-      val cols: Seq[String] = z.next.get
-      cols.size === 2
-      cols(0) === "abc"
-      cols(1) === "def"
-      z.hasNext === false
-    }
-
-    "parse one line with crlf" in {
-      val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,def\r\n".toIterator)
-      z.hasNext === true
-      val cols: Seq[String] = z.next.get
-      cols.size === 2
-      cols(0) === "abc"
-      cols(1) === "def"
-      z.hasNext === false
-    }
-
-    "parse one line with lf" in {
-      val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,def\n".toIterator)
-      z.hasNext === true
-      val cols: Seq[String] = z.next.get
-      cols.size === 2
-      cols(0) === "abc"
-      cols(1) === "def"
-      z.hasNext === false
-    }
-
-    "parse one line with quote and crlf" in {
-      val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,\"def\"\r\n".toIterator)
-      z.hasNext === true
-      val cols: Seq[String] = z.next.get
-      cols.size === 2
-      cols(0) === "abc"
-      cols(1) === "def"
-      z.hasNext === false
-    }
-
-    "parse one line with quote and lf" in {
-      val z: Iterator[Try[Seq[String]]] = Parser.parseLines("abc,\"def\"\n".toIterator)
-      z.hasNext === true
-      val cols: Seq[String] = z.next.get
-      cols.size === 2
-      cols(0) === "abc"
-      cols(1) === "def"
-      z.hasNext === false
-    }
-
-    "parse empty line" in {
-      val z: Iterator[Try[Seq[String]]] = Parser.parseLines("\r\n".toIterator)
-      z.hasNext === true
-      val cols: Seq[String] = z.next.get
-      cols.size === 1
-      cols(0) === ""
-      z.hasNext === false
-    }
-
-    "parse col with crlf" in {
-      val z: Iterator[Try[Seq[String]]] = Parser.parseLines("\"ab\r\ncd\",b".toIterator)
-      z.hasNext === true
-      val cols: Seq[String] = z.next.get
-      cols.size === 2
-      cols(0) === "ab\r\ncd"
-      cols(1) === "b"
-      z.hasNext === false
-    }
-
-    "parse two lines" in {
-      val z: Iterator[Try[Seq[String]]] = Parser.parseLines("\"ab\r\ncd\",b\r\n\"de f\"".toIterator)
-      z.hasNext === true
-
-      doWith(z.next.get) { cols =>
-        cols.size === 2
-        cols(0) === "ab\r\ncd"
-        cols(1) === "b"
+      assert(z.hasNext === true)
+      z.next match {
+        case s: Success[Seq[String]] => fail
+        case f: Failure[Seq[String]] => {
+          assert(f.exception.asInstanceOf[CsvParseException].lineNo === 2)
+        }
       }
 
-      z.hasNext === true
-      doWith(z.next.get) { cols =>
-        cols.size === 1
-        cols(0) === "de f"
-      }
+      assert(z.hasNext === false)
+    }
+    t.get
+    assert(t.isSuccess === true)
+  }
 
-      z.hasNext === false
+  it should "parse with header when empty" in {
+    import com.ruimo.csv.Parser._
+
+    assert(asHeaderedCsv(Iterator.empty).hasNext === false)
+  }
+
+  it should "parse with header when error" in {
+    import com.ruimo.csv.Parser._
+
+    val it = asHeaderedCsv(Iterator.single(Failure(new Exception("Error"))))
+    assert(it.hasNext === true)
+    it.next() match {
+      case Success(_) => fail
+      case Failure(e) => assert(e.getMessage === "Error")
+      }
+  }
+
+  it should "parse csv having only header" in {
+    import com.ruimo.csv.Parser._
+
+    val it = asHeaderedCsv(Iterator.single(Success(Seq("Hello", "World"))))
+    assert(it.hasNext === false)
+  }
+
+  it should "parse csv read error" in {
+    import com.ruimo.csv.Parser._
+
+    val it = asHeaderedCsv(Iterator(
+      Success(Seq("Hello", "World")),
+      Failure(new Exception("Error"))
+    ))
+    assert(it.hasNext === true)
+    it.next() match {
+      case Success(_) => fail
+        case Failure(e) => assert(e.getMessage === "Error")
+    }
+  }
+
+  it should "parse csv having two lines" in {
+    import com.ruimo.csv.Parser._
+
+    val it = asHeaderedCsv(Iterator(
+      Success(Seq("Name", "Age")),
+      Success(Seq("Ruimo", "18")),
+      Success(Seq("Uno", "16"))
+    ))
+    assert(it.hasNext === true)
+    doWith(it.next().get) { l =>
+      assert(l.lineNo === 2)
+      assert(l.header.header.size === 2)
+      assert(l('Name) === "Ruimo")
+      assert(l('Age) === "18")
     }
 
-    "parse empty file" in {
-      import com.ruimo.scoins.LoanPattern.iteratorFromReader
-      import com.ruimo.csv.Parser.parseLines
-
-      val file = Files.createTempFile(null, ".csv")
-      val t = iteratorFromReader(Files.newBufferedReader(file, Charset.forName("Windows-31j"))) {
-        in: Iterator[Char] => 
-          val z: Iterator[Try[Seq[String]]] = parseLines(in)
-          z.hasNext === false
-      }
-      t.isSuccess === true
+    assert(it.hasNext === true)
+    doWith(it.next().get) { l =>
+      assert(l.lineNo === 3)
+      assert(l.header.header.size === 2)
+      assert(l('Name) === "Uno")
+      assert(l('Age) === "16")
     }
 
-    "parse blank line" in {
-      import com.ruimo.scoins.LoanPattern.iteratorFromReader
-      import com.ruimo.csv.Parser.parseLines
-      import java.util.Arrays.asList
+    assert(it.hasNext === false)
+  }
 
-      val file = Files.createTempFile(null, ".csv")
-      Files.write(file, asList("", ""), Charset.forName("Windows-31j"))
-      val t = iteratorFromReader(Files.newBufferedReader(file, Charset.forName("Windows-31j"))) {
-        in: Iterator[Char] => 
-          val z: Iterator[Try[Seq[String]]] = parseLines(in)
-          z.hasNext === true
-          z.next.get === Seq("")
+  it should "parse csv having header line is ok and second line is ng" in {
+    import com.ruimo.csv.Parser._
 
-          z.hasNext === true
-          z.next.get === Seq("")
-
-          z.hasNext === false
-      }
-      t.isSuccess === true
+    val it = asHeaderedCsv(Iterator(
+      Success(Seq("Name", "Age")),
+      Success(Seq("Ruimo", "18")),
+      Failure(new Exception("Error"))
+    ))
+    assert(it.hasNext === true)
+    doWith(it.next().get) { l =>
+      assert(l.lineNo === 2)
+      assert(l.header.header.size === 2)
+      assert(l('Name) === "Ruimo")
+      assert(l('Age) === "18")
     }
 
-    "parse normal lines" in {
-      import com.ruimo.scoins.LoanPattern.iteratorFromReader
-      import com.ruimo.csv.Parser.parseLines
-      import java.util.Arrays.asList
-
-      val file = Files.createTempFile(null, ".csv")
-      Files.write(file, asList("a,\"b\"", "1,\"2 3\""), Charset.forName("Windows-31j"))
-      val t = iteratorFromReader(Files.newBufferedReader(file, Charset.forName("Windows-31j"))) {
-        in: Iterator[Char] => 
-          val z: Iterator[Try[Seq[String]]] = parseLines(in)
-          z.hasNext === true
-          z.next.get === Seq("a", "b")
-
-          z.hasNext === true
-          z.next.get === Seq("1", "2 3")
-
-          z.hasNext === false
-      }
-      t.isSuccess === true
+    assert(it.hasNext === true)
+    it.next() match {
+      case Success(_) => fail
+        case Failure(e) => assert(e.getMessage === "Error")
     }
 
-    "parse error" in {
-      import com.ruimo.scoins.LoanPattern.iteratorFromReader
-      import com.ruimo.csv.Parser.parseLines
-      import java.util.Arrays.asList
-
-      val file = Files.createTempFile(null, ".csv")
-      Files.write(file, asList("a,\"b\"", "1,\"2 3"), Charset.forName("Windows-31j"))
-      val t = iteratorFromReader(Files.newBufferedReader(file, Charset.forName("Windows-31j"))) {
-        in: Iterator[Char] => 
-          val z: Iterator[Try[Seq[String]]] = parseLines(in)
-          z.hasNext === true
-          z.next.get === Seq("a", "b")
-
-          z.hasNext === true
-          z.next match {
-            case s: Success[Seq[String]] => failure
-            case f: Failure[Seq[String]] => {
-              f.exception.asInstanceOf[CsvParseException].lineNo === 2
-            }
-          }
-
-          z.hasNext === false
-      }
-      t.get
-      t.isSuccess === true
-    }
-
-    "parse with header when empty" in {
-      import com.ruimo.csv.Parser._
-
-      asHeaderedCsv(Iterator.empty).hasNext === false
-    }
-
-    "parse with header when error" in {
-      import com.ruimo.csv.Parser._
-
-      val it = asHeaderedCsv(Iterator.single(Failure(new Exception("Error"))))
-      it.hasNext === true
-      it.next() match {
-        case Success(_) => failure
-        case Failure(e) => e.getMessage === "Error"
-      }
-      1 === 1
-    }
-
-    "parse csv having only header" in {
-      import com.ruimo.csv.Parser._
-
-      val it = asHeaderedCsv(Iterator.single(Success(Seq("Hello", "World"))))
-      it.hasNext === false
-    }
-
-    "parse csv read error" in {
-      import com.ruimo.csv.Parser._
-
-      val it = asHeaderedCsv(Iterator(
-        Success(Seq("Hello", "World")),
-        Failure(new Exception("Error"))
-      ))
-      it.hasNext === true
-      it.next() match {
-        case Success(_) => failure
-        case Failure(e) => e.getMessage === "Error"
-      }
-      1 === 1
-    }
-
-    "parse csv having two lines" in {
-      import com.ruimo.csv.Parser._
-
-      val it = asHeaderedCsv(Iterator(
-        Success(Seq("Name", "Age")),
-        Success(Seq("Ruimo", "18")),
-        Success(Seq("Uno", "16"))
-      ))
-      it.hasNext === true
-      doWith(it.next().get) { l =>
-        l.lineNo === 2
-        l.header.header.size === 2
-        l('Name) === "Ruimo"
-        l('Age) === "18"
-      }
-
-      it.hasNext === true
-      doWith(it.next().get) { l =>
-        l.lineNo === 3
-        l.header.header.size === 2
-        l('Name) === "Uno"
-        l('Age) === "16"
-      }
-
-      it.hasNext === false
-    }
-
-    "parse csv having header line is ok and second line is ng" in {
-      import com.ruimo.csv.Parser._
-
-      val it = asHeaderedCsv(Iterator(
-        Success(Seq("Name", "Age")),
-        Success(Seq("Ruimo", "18")),
-        Failure(new Exception("Error"))
-      ))
-      it.hasNext === true
-      doWith(it.next().get) { l =>
-        l.lineNo === 2
-        l.header.header.size === 2
-        l('Name) === "Ruimo"
-        l('Age) === "18"
-      }
-
-      it.hasNext === true
-      it.next() match {
-        case Success(_) => failure
-        case Failure(e) => e.getMessage === "Error"
-      }
-
-      it.hasNext === false
-    }
+    assert(it.hasNext === false)
   }
 }
